36dc38a30a35b23d6d4dfd3a23c2e195a8ce2f6c
Teun Luijken <teunluijken+gitkraken@gmail.com>
2021-02-05 14:42:41 +0100

    opdracht 7

---
+++ week1opdracht7a/week1opdracht7a/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/5/2021 2:40:01 PM
+ *  Author: teunl
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
